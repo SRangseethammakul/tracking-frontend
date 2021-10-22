1df53176ae233de18d83f75f52c9af40958e3cdc
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-10-23 00:38:24 +0700

    check exp date

--- src/guard/admin.js
+++ src/guard/admin.js
@@ -1,18 +1,28 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, useHistory } from "react-router-dom";
+import { updateProfile } from "../redux/actions/authAction";
+import { useDispatch } from "react-redux";
 import jwt from "jsonwebtoken";
 export default function PrivateRoute({ children, ...rest }) {
   let isAuth = false;
   const token = JSON.parse(localStorage.getItem("token"));
+  const history = useHistory();
+  const dispatch = useDispatch();
   const decode1 = token ? jwt.decode(token.access_token) : null;
-  if (token && decode1.role === 'admin') {
+  if (token && decode1.role === "admin") {
     isAuth = true;
+  }
+  if (token && Date.now() >= token.expires_in * 1000) {
+    localStorage.removeItem("token");
+    localStorage.removeItem("profile");
+    history.replace("/");
+    dispatch(updateProfile(null));
   }
   return (
     <Route
       {...rest}
       render={({ location }) =>
-      isAuth ? (
+        isAuth ? (
           children
         ) : (
           <Redirect
