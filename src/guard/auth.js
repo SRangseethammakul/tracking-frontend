ce18245f36c88623b015138a6305f43a12e6618b
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-10-25 21:51:14 +0700

    🔒️ add check 401

--- src/guard/auth.js
+++ src/guard/auth.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect,useHistory } from "react-router-dom";
+import { updateProfile } from "../redux/actions/authAction";
+import { useDispatch } from "react-redux";
 export default function PrivateRoute({ children, ...rest }) {
   let isAuth = false;
   const token = JSON.parse(localStorage.getItem("token"));
+  const history = useHistory();
+  const dispatch = useDispatch();
   if (token) {
     isAuth = true;
   }
